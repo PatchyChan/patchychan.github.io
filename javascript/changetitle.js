873283a8e05473d90de6eb046a7a8b4164738a7f
PatchyChan <patchouli251304@gmail.com>
2020-11-12 15:52:47 +0800

    Site updated: 2020-11-12 15:52:46

--- javascript/changetitle.js
+++ javascript/changetitle.js
@@ -2,7 +2,7 @@ var origintitle = document.title
 
 document.addEventListener('visibilitychange', function() {
     if (document.visibilityState=='hidden') {//切离该页面时执行
-      document.title = "Wait for your return"   
+      document.title = "UwU"   
     } 
     else if(document.visibilityState=='visible') {//切换到该页面时执行
       document.title = origintitle
