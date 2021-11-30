5891ff1370330634e238bc47fb45b870e45d2918
Patchy Chan <email@example.com>
2021-11-30 21:52:40 +0800

    Site updated: 2021-11-30 21:52:40

--- javascript/changetitle.js
+++ javascript/changetitle.js
@@ -1,11 +1,11 @@
-var origintitle = document.title
+var origintitle = document.title;
 
 document.addEventListener('visibilitychange', function() {
     if (document.visibilityState=='hidden') {
-      document.title = "UwU"   
+      document.title = "UwU";
     } 
     else if(document.visibilityState=='visible') {
-      document.title = origintitle
+      document.title = origintitle;
     }
   }
 );
