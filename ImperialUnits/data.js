3a70ed0a7460c166e8aa23b0b214dc7bdf9be7d5
PatchyChan <PatchyChan@users.noreply.github.com>
2023-02-04 07:57:08 +0000

    deploy: PatchyChan/loft-source@e8b1893976ec3d5b8e26920a0120d08bf205ebd8

--- ImperialUnits/data.js
+++
@@ -1,4 +0,0 @@
-var length_units = new Array(
-    {name: "inch", text: "1inch = 25.4cm" }, 
-    {name: "foot", text: "1foot = 30.48cm = 12inch" },
-    {name: "yard", text: "1yard = 91.44cm = 36inch" });
